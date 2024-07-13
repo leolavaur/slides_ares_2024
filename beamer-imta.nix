eadbb088c87efaff027e93250f7a49f4cdb92efe
phdcybersec <82591009+phdcybersec@users.noreply.github.com>
2024-07-13 16:42:55 +0200

    :sparkles: Initial version, presented at the SOTERN team seminar.

---
+++ beamer-imta.nix
@@ -0,0 +1,42 @@
+{ lib
+, stdenv, fetchgit, writeShellScript
+, texlive
+}:
+
+stdenv.mkDerivation rec {
+  # Make this a valid tex(live-new) package;
+  # the pkgs attribute is provided with a hack below.
+  pname = "beamer-imta";
+  version = "v1.0";
+  tlType = "run";
+
+  outputs = [ "tex" ];
+
+  src = ./beamer;
+
+  nativeBuildInputs = [
+    (writeShellScript "force-tex-output.sh" ''
+        out="''${tex-}"
+      '')
+    kpsewhich
+  ];
+
+  dontConfigure = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    path="$tex/tex/latex/${pname}"
+    mkdir -p "$path"
+    cp **/*.{cls,def,clo,sty} "$path/"
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    homepage = "https://https://gitlab.com/latex-imt-atlantique/beamer-imt-atlantique";
+    description = "IMT Atlantique theme for beamer";
+    # license = licenses.lppl13c;
+    platforms = platforms.all;
+  };
+}
